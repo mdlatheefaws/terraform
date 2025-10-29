0ed7fcf5c9830a6bd3c40305127ed080225da0e9
mdlatheefaws <mdlatheefaws@gmail.com>
2025-10-29 12:54:53 +0530

    s3.tf.old

--- s3backend.tf
+++ s3backend.tf
@@ -1,27 +1,10 @@
-# terraform {
-#   backend "s3" {
-#     bucket         = "ladsoftterarforms3"
-#     key            = "prod/terraform.tfstate"
-#     region         = "ap-south-1"
-#     encrypt        = true
-#     profile = "default"
-#   }
-# }
-
-
 terraform {
   backend "s3" {
-    bucket               = "ladsoftawss3bucket"
+    bucket               = "ladsoftterarforms3"
     region               = "ap-south-1"
-    encrypt              = true
-
-    # This 'key' is the filename used inside each workspace folder
-    key                  = "terraform.tfstate"
-
-    # Each workspace gets its own folder: state/<workspace>/terraform.tfstate
-    workspace_key_prefix = "state"
-
-    # Optional: if you want Terraform to use a specific AWS CLI profile
-    # profile = "s3"
+    #encrypt              = true
+    key                  = "prod/terraform.tfstate"
   }
 }
+
+
